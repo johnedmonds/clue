4c3a0b9b4ab7258d94971c5adc35a2ec1b1a4727
John Edmonds <pocketcookies2@gmail.com>
2010-08-08 13:13:17 -0400

    Proving we can read from and write to the client.

--- clue-mud/src/main/java/com/pocketcookies/clue/service/server/mud/MudPlayer.java
+++ clue-mud/src/main/java/com/pocketcookies/clue/service/server/mud/MudPlayer.java
@@ -1,6 +1,8 @@
 package com.pocketcookies.clue.service.server.mud;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -10,6 +12,8 @@ public class MudPlayer implements Runnable {
 
 	private Socket client;
 	private static final Logger logger = Logger.getLogger(MudPlayer.class);
+	private PrintWriter writer;
+	private BufferedReader reader;
 
 	public MudPlayer(Socket client) {
 		this.client = client;
@@ -18,8 +22,13 @@ public class MudPlayer implements Runnable {
 	@Override
 	public void run() {
 		try {
-			PrintWriter writer = new PrintWriter(client.getOutputStream());
-			writer.println("Hello client.  This is just a test to see if everything worked so we're going to close the connection now.");
+			writer = new PrintWriter(client.getOutputStream());
+			reader = new BufferedReader(new InputStreamReader(
+					client.getInputStream()));
+			writer.println("Hello client.  This is just a test to see if everything worked.  Send us something and we'll tell you what you sent.  Then we'll close the connection.");
+			writer.flush();
+			writer.println("You said: \"" + reader.readLine()
+					+ ".\"  Have a good day.");
 			writer.flush();
 			writer.close();
 			client.close();
