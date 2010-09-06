fa16f8d99ece12b6c4ebe67b30452add5a1a4508
John Edmonds <pocketcookies2@gmail.com>
2010-09-06 19:42:15 -0400

    Lower case now works too.
    --HG--
    branch : boardroom

--- clue-mud/src/main/java/com/pocketcookies/clue/service/server/mud/commands/MoveCommand.java
+++ clue-mud/src/main/java/com/pocketcookies/clue/service/server/mud/commands/MoveCommand.java
@@ -1,7 +1,7 @@
 package com.pocketcookies.clue.service.server.mud.commands;
 
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -19,17 +19,20 @@ public class MoveCommand implements Command {
 
 	@Override
 	public Collection<String> getCommandAliases() {
-		return Collections.unmodifiableCollection(Arrays.asList(new String[] {
-				"n", "s", "e", "w", "ne", "nw", "se", "sw", "north",
-				"northeast", "northwest", "southwest", "southeast", "south",
-				"east", "west" }));
+		ArrayList<String> tempCommands = new ArrayList<String>(
+				Room.values().length);
+		for (Room r : Room.values()) {
+			tempCommands.add(r.toString());
+			tempCommands.add(r.toString().toLowerCase());
+		}
+		return Collections.unmodifiableCollection(tempCommands);
 	}
 
 	@Override
 	public void process(String command, MudPlayer player) {
 		PrintWriter writer = player.getWriter();
 		try {
-			Room to = Room.valueOf(command.split(" ")[1]);
+			Room to = Room.valueOf(command.toUpperCase());
 			player.getService().move(player.getKey(), player.getGameId(), to);
 			// There is no need to move the player here because if the move is
 			// successful, we will get a message which will cause us to position
