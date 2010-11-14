38b71021ab3af871e0ed31cf388615f723bde40e
Jack Edmonds <pocketcookies2@gmail.com>
2010-11-14 15:20:13 -0500

    Branching to start using Spring to handle the requests in clue-web.
    --HG--
    branch : clue-web-spring
    rename : clue-web/src/main/java/com/pocketcookies/clue/service/server/web/ClueWebServlet.java => clue-web/src/main/java/com/pocketcookies/clue/service/server/web/ClueWebController.java
    rename : clue-web/src/main/webapp/index.jsp => clue-web/src/main/webapp/WEB-INF/jsp/index.jsp

---
+++ clue-web/src/main/java/com/pocketcookies/clue/service/server/web/ClueWebController.java
@@ -0,0 +1,40 @@
+package com.pocketcookies.clue.service.server.web;
+
+import java.net.MalformedURLException;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.caucho.hessian.client.HessianProxyFactory;
+import com.pocketcookies.clue.config.Config;
+import com.pocketcookies.clue.service.server.ClueServiceAPI;
+
+/**
+ * Servlet implementation class ClueWebServlet
+ */
+@Controller
+public class ClueWebController {
+	private static final long serialVersionUID = 1L;
+	private final ClueServiceAPI service;
+	private static final Logger logger = Logger
+			.getLogger(ClueWebController.class);
+
+	public ClueWebController() {
+		super();
+		try {
+			service = (ClueServiceAPI) new HessianProxyFactory().create(
+					ClueServiceAPI.class, Config.SERVICE_LOCATION);
+		} catch (MalformedURLException e) {
+			logger.fatal("There was a problem connecting to the service.", e);
+			throw new ExceptionInInitializerError(e);
+		}
+		System.out.println("Here");
+	}
+
+	@RequestMapping(value = "/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+}
